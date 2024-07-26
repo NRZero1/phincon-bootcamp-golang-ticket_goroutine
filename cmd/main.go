f2e0da5d7fca1ae66de81bc5a4972db6fed19f2a
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-26 17:18:12 +0700

    feat(middleware): add handler for collection of middleware

--- cmd/main.go
+++ cmd/main.go
@@ -12,9 +12,13 @@ func main() {
 	// uc := usecase.NewBookUseCase(r)
 	// h := handler.NewBookHandler(uc)
 
+	middlewares := middleware.CreateStack(
+		middleware.Logging,
+	)
+	
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: middleware.Logging(router),
+		Handler: middlewares(router),
 	}
 
 	fmt.Println("Server is running in port ", server.Addr)
