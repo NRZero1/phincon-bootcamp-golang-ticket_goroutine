dda19e5652f4c78f724eb002b2fb9c622df59585
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-26 18:16:55 +0700

    feat(cmd): add event endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"ticket_goroutine/internal/handler"
+	handlerImplement "ticket_goroutine/internal/handler/impl"
 	"ticket_goroutine/internal/middleware"
+	repoImplement "ticket_goroutine/internal/repository/impl"
+	useCaseImplement "ticket_goroutine/internal/usecase/impl"
 )
+
+var eventHandler handler.EventHandlerInterface
+
+func initEventHandler() handler.EventHandlerInterface {
+	repo := repoImplement.NewEventRepository()
+	usecase := useCaseImplement.NewEventUseCase(repo)
+	handler := handlerImplement.NewEventHandler(usecase)
+	return handler
+}
+
+func init() {
+	eventHandler = initEventHandler()
+}
 
 func main() {
 	router := middleware.NewRouter()
-	// r := repository.NewBookRepository()
-	// uc := usecase.NewBookUseCase(r)
-	// h := handler.NewBookHandler(uc)
+
+	router.AddRoute("GET", "/event/", eventHandler.GetAll)
+	router.AddRoute("GET", "/event/{id}", eventHandler.FindById)
+	router.AddRoute("POST", "/event/", eventHandler.Save)
 
 	middlewares := middleware.CreateStack(
 		middleware.Logging,
