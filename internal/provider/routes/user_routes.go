6be82ac23d4130b93f78c050dd8b443e5604fc13
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-08-05 11:38:14 +0700

    fix: typo from eventhandlerinterface to userhandlerinterface

--- internal/provider/routes/user_routes.go
+++ internal/provider/routes/user_routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func UserRoutes(routerGroup *gin.RouterGroup, userHandler handler.EventHandlerInterface) {
+func UserRoutes(routerGroup *gin.RouterGroup, userHandler handler.UserHandlerInterface) {
 	routerGroup.GET("/", userHandler.GetAll)
 	routerGroup.GET("/:id", userHandler.FindById)
 	routerGroup.POST("/", userHandler.Save)
